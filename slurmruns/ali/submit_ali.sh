2f7c194182219e4ee4ecbe99cd4828a9e42f8d87
willjardee <willjardee@gmail.com>
2025-01-24 15:12:32 -0700

    slurm jobs

---
+++ slurmruns/ali/submit_ali.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+sbatch tuning_ali_aco.sbatch
+sbatch tuning_ali_mmaco.sbatch
+sbatch tuning_ali_antq.sbatch
+sbatch tuning_ali_adaco.sbatch
+sbatch tuning_ali_acopg-clip.sbatch
+sbatch tuning_ali_acopg.sbatch
+sbatch tuning_ali_acosgd.sbatch
+sbatch tuning_ali_acosgd-l2.sbatch
---
+++ slurmruns/att/submit_ali.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+sbatch tuning_att_aco.sbatch
+sbatch tuning_att_mmaco.sbatch
+sbatch tuning_att_antq.sbatch
+sbatch tuning_att_adaco.sbatch
+sbatch tuning_att_pgaco-ratio-clip.sbatch
